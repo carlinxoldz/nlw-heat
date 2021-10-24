2467c1da28dcb9cdfc3b2ba2c5208cf245526f65
calinxoldz <carlinxold@hotmail.com>
2021-10-24 12:55:06 -0300

    1

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle)
+
+
+
+export {router};
